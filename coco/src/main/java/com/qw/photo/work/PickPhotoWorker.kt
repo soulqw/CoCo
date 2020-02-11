dd2581877225fc5d75013ef71ec9e55322e4281b
longshihan <577093937@qq.com>
2020-02-11 22:23:17 +0800

    修改从文件选择器加载图片崩溃的问题

--- coco/src/main/java/com/qw/photo/work/PickPhotoWorker.kt
+++ coco/src/main/java/com/qw/photo/work/PickPhotoWorker.kt
@@ -2,6 +2,8 @@ package com.qw.photo.work
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import android.provider.MediaStore
 import android.text.TextUtils
 import com.qw.photo.Utils
@@ -11,6 +13,10 @@ import com.qw.photo.constant.Constant
 import com.qw.photo.exception.BaseException
 import com.qw.photo.pojo.PickParams
 import com.qw.photo.pojo.PickResult
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
 
 /**
  * Created by rocket on 2019/6/18.
@@ -24,7 +30,8 @@ class PickPhotoWorker(handler: IAcceptActivityResultHandler) :
     }
 
     private fun pickPhoto(activity: Activity, callBack: GetImageCallBack<PickResult>) {
-        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        val pickIntent = Intent(Intent.ACTION_GET_CONTENT, null)
+        pickIntent.type = "image/*"
         if (null === pickIntent.resolveActivity(activity.packageManager)) {
             callBack.onFailed(BaseException("activity status error"))
             return
@@ -59,9 +66,28 @@ class PickPhotoWorker(handler: IAcceptActivityResultHandler) :
                 try {
                     localPath = Utils.uriToImagePath(activity, intentData.data!!)
                 } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                try {
+                    if (TextUtils.isEmpty(localPath)) {
+                        val uri = intentData.data!!
+                        var bitmap =
+                            MediaStore.Images.Media.getBitmap(activity.getContentResolver(), uri)
+                        if (bitmap==null) return
+                        bitmap=getPerfectBitmap(bitmap,800)
+                        var file: File =
+                            File(activity.externalCacheDir.path + "/" + System.currentTimeMillis() + ".jpg")
+                        if (!file.exists()) file.createNewFile()
+                        localPath = file.path
+                        val fos = FileOutputStream(file)
+                        fos.write(Bitmap2Bytes(bitmap))
+                        fos.close()
+                    }
+                }catch (e:Exception){
                     callBack.onFailed(e)
                 }
                 localPath ?: return
+                result.targetFile = File(localPath)
                 if (!TextUtils.isEmpty(localPath) && null != mParams.disposer) {
                     Utils.disposeImage(
                         mHandler.getLifecycleHost(),
@@ -78,6 +104,37 @@ class PickPhotoWorker(handler: IAcceptActivityResultHandler) :
         } else {
             callBack.onFailed(NullPointerException("null result intentData"))
         }
+    }
+
+    fun Bitmap2Bytes(bm: Bitmap): ByteArray? {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 60, baos)
+        return baos.toByteArray()
+    }
+
+    fun getPerfectBitmap(sourceBitmap: Bitmap, maxSize: Int): Bitmap? {
+        val bitmap: Bitmap?
+        if (sourceBitmap.width > maxSize || sourceBitmap.height > maxSize) {
+            bitmap = if (sourceBitmap.width >= sourceBitmap.height) {
+                val beli = sourceBitmap.width / maxSize.toFloat()
+                val heighttemp = (sourceBitmap.height / beli).toInt()
+                ThumbnailUtils.extractThumbnail(sourceBitmap, maxSize, heighttemp)
+            } else {
+                val beli = sourceBitmap.height / maxSize.toFloat()
+                val widthtemp = (sourceBitmap.width / beli).toInt()
+                ThumbnailUtils.extractThumbnail(sourceBitmap, widthtemp, maxSize)
+            }
+            /**
+             * 因为bitmap = ThumbnailUtils.extractThumbnail(bitmaptemp, 1024,
+             * heighttemp);中的bitmaptemp和bitmap已经不是指向 同一个内存区域，所以需要回收bitmaptemp。
+             */
+            if (sourceBitmap != null && !sourceBitmap.isRecycled) {
+                sourceBitmap.recycle()
+            }
+        } else {
+            bitmap = sourceBitmap
+        }
+        return bitmap
     }
 
 }
